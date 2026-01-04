2d43c1d6290184e5b340ebd1a05c00f05c103d7a
ARIN KIM <67595512+rin-k645@users.noreply.github.com>
2026-01-04 15:50:46 +0900

    :tada: add: init project

---
+++ spring-board/src/test/java/io/github/rink645/springboard/SpringBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rink645.springboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
